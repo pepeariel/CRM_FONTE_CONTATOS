5500301b0ec46129d1f92fd767159afe8cb4bedf
pepeariel <peedroariel@gmail.com>
2022-10-03 11:04:48 -0300

    gitignore_removed

--- duplicates.py
+++ duplicates.py
@@ -1,14 +1,19 @@
+import ssl
 from db_conection import create_connection
 import pandas as pd
+import requests
 
 SECRET_DB_PASSOWRD = "3xpdOcTa0HlIjG"
-SECRET_API_KEY = "711ad004c12-c61b-42ef-b95a-770c00fd5863"
+
 
 engine, con = create_connection('CRM.db', SECRET_DB_PASSOWRD)
+
+df_fonte = pd.read_sql('SELECT * FROM fonte', con)
 
 # Requisição dos ids duplicados
 #ids = df_fonte['idinterno']
 #df_duplicados = df_fonte[ids.isin(ids[ids.duplicated()])].sort_values('idinterno')
+#print(df_duplicados)
 
 
 
